1f8150adfd653be1020f52031172ec46afd9e227
Alex <jalexpayan@gmail.com>
2024-12-03 20:34:10 -0800

    renombrar imagenes

--- MagnoliaEventos/LandingPage.Designer.cs
+++ MagnoliaEventos/LandingPage.Designer.cs
@@ -100,7 +100,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::MagnoliaEventos.Properties.Resources._3;
+            this.BackgroundImage = global::MagnoliaEventos.Properties.Resources.Bienvenido;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1516, 901);
             this.Controls.Add(this.btnReportesI);
--- MagnoliaEventos/Log in.Designer.cs
+++ MagnoliaEventos/Log in.Designer.cs
@@ -49,7 +49,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::MagnoliaEventos.Properties.Resources._1;
+            this.BackgroundImage = global::MagnoliaEventos.Properties.Resources.Inicio;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1516, 901);
             this.Controls.Add(this.btnSesión);
